6c7b5163c81a780a8c4ff8abb2808bb24b42af3d
Me <dima.kutuzov@gmail.com>
2021-09-20 19:46:40 +0300

    update

--- main.py
+++ main.py
@@ -39,23 +39,54 @@ class CatalogData:
 
 def WriteFile():
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-
-    catalog_data = CatalogData()
+    dt_string = now.strftime("%Y-%m-%d %H:%M")
 
     data = ET.Element('yml_catalog')
     data.set('date', dt_string)
     shop = ET.SubElement(data, 'shop')
     name = ET.SubElement(shop, 'name')
+    name.text = catalog_data.name
     company = ET.SubElement(shop, 'company')
+    company.text = catalog_data.company
     url = ET.SubElement(shop, 'url')
-    currencies =ET.SubElement(shop, 'currencies')
+    url.text = catalog_data.url
+    currencies = ET.SubElement(shop, 'currencies')
     currency = currencies.makeelement('currency', catalog_data.currency)
     currencies.append(currency)
     categories = ET.SubElement(shop, 'categories')
+    for cat in catalog_data.categories:
+        category = ET.SubElement(categories, 'category')
+        category.set('id', cat[0])
+        category.text = cat[1]
 
     mydata = minidom.parseString(ET.tostring(data)).toprettyxml(indent = '    ')
     with open ('test.xml', 'w', encoding='utf-8') as test:
         test.write(mydata)
 
+
+def ReadFromFiles(offers_xml, import_xml):
+
+    offerslist = []
+    categories_list = []
+
+    offers_et = ET.parse(offers_xml)
+    offers_root = offers_et.getroot()
+
+    import_et = ET.parse(import_xml)
+    import_root = import_et.getroot()
+
+    def FindCategories(grougs):
+        categories_list.append([grougs[0][0].text , grougs[0][1].text])
+        try:
+            groups = grougs[0][2]
+            FindCategories(groups)
+        except:
+            pass
+
+    FindCategories(import_root[0][3])
+    catalog_data.categories = categories_list
+
+catalog_data = CatalogData()
+
+ReadFromFiles('webdata/offers0_1.xml','webdata/import0_1.xml')
 WriteFile()
