112496ac461fbad6552a81c7eaefe28a3985c906
Me <dima.kutuzov@gmail.com>
2021-10-05 18:48:51 +0300

    add asserts, switch search of tags to .find()

--- reader.py
+++ reader.py
@@ -12,6 +12,7 @@ def collect_data(list_of_pairs, catalog_data):
     # for every "import.xml" file
     # if obj.categories doesnt already content it
     def find_categories(groups):
+        assert groups.tag != '{urn:1C.ru:commerceml_2}Группы'
         category_data = groups[0][0].text, groups[0][1].text
         if category_data not in catalog_data.categories:
             catalog_data.categories.append(category_data)
@@ -33,19 +34,18 @@ def collect_data(list_of_pairs, catalog_data):
         find_categories(import_root[0][3])
 
         def check_offerpic(product):
-            if product.findall('{urn:1C.ru:commerceml_2}Картинка') == []:
-                return False
-            else:
+            if product.findall('{urn:1C.ru:commerceml_2}Картинка') != []:
                 return True
 
         def check_description(product):
-            if product[5].text[-3:] == 'DEL':
-                return False
-            else:
-                return True
+            description = product.find('{urn:1C.ru:commerceml_2}Описание')
+            if description is not None:
+                if len(description.text) > 0:
+                    if description.text[-3:] != 'DEL':
+                        return True
 
         def size_finder(product_name):
-            size_data = re.search(r'Р(\w[\w]*)([-/])?(\w[\w]*)?\)', product_name)
+            size_data = re.search(r'Р(\w[\w]*)([-\/])?(\w[\w]*)?\)', product_name)
             mid_symbol = size_data.groups()[1]
             first_size = size_data.groups()[0]
             last_size = size_data.groups()[2]
@@ -79,30 +79,34 @@ def collect_data(list_of_pairs, catalog_data):
             for product_obj in product_obj_set.values():
                 product_obj.sizes_available = dict_of_available[product_obj.id]
 
-
-                    # This code works with importX_X input file
+        # This code works with importX_X input file
         # It creates OfferData() objs dynamically with "product_set" dict
         # and sets most of their attribs by parsing importX_X
         # products: <Товары> tag in input file
         products = import_root[1][4]
+        assert products.tag != '{urn:1C.ru:commerceml_2}Группы'
         product_set = {}  # offerId : OfferData() obj
         for product in products:
             if check_offerpic(product) and check_description(product):
-                offerId = product[0].text
+                offerId = product.find('{urn:1C.ru:commerceml_2}Ид').text
                 product_set[offerId] = OfferData()
                 product_set[offerId].offerId = offerId
-                product_set[offerId].vendorCode = product[1].text
-                name = product[2].text
+                product_set[offerId].vendorCode = product.find('{urn:1C.ru:commerceml_2}Артикул').text
+                name = product.find('{urn:1C.ru:commerceml_2}Наименование').text
                 # delete size information from name
-                name = re.sub(r' \(Р(\w[\w]*)([-/])?(\w[\w]*)?\)', '', name)
+                name = re.sub(r' *\( *[\w]* *Р *(\w[\w]*) *([-\/])? *(\w[\w]*)?\)', '', name)
                 product_set[offerId].name = name
                 product_set[offerId].categoryId = import_root[0][3][0][2][0][0].text
-                product_set[offerId].vendor_id = product[4][0].text
-                product_set[offerId].description = product[5].text
+                assert not re.match(r'[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}', product_set[offerId].categoryId)
+                product_set[offerId].vendor_id = product.find('{urn:1C.ru:commerceml_2}Группы')[0].text
+                assert not re.match(r'[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}', product_set[offerId].vendor_id)
+                product_set[offerId].description = product.find('{urn:1C.ru:commerceml_2}Описание').text
                 pictures = product.findall('{urn:1C.ru:commerceml_2}Картинка')
                 for picture in pictures:
                     product_set[offerId].pictures.append('https://www.polyanka.pl/yml/webdata/' + picture.text)
-                product_id = product.find('{urn:1C.ru:commerceml_2}ЗначенияРеквизитов')[0][1].text[2:]
+                product_id = product.find('{urn:1C.ru:commerceml_2}ЗначенияРеквизитов')[0][1].text
+                assert product.find('{urn:1C.ru:commerceml_2}ЗначенияРеквизитов')[0][0].text != 'Код'
+                product_id = re.search(r'[\d]+', product_id).group()
                 product_set[offerId].id = product_id
                 for category in catalog_data.categories:
                     if category[0] == product_set[offerId].vendor_id:
@@ -115,16 +119,17 @@ def collect_data(list_of_pairs, catalog_data):
         # 3) set attribs to suitable OfferData() obj
         # offers: <Предложения> tag in input file
         offers = offers_root[1][7]
+        assert offers.tag != '{urn:1C.ru:commerceml_2}Предложения'
         for offer in offers:
-            offerId = offer[0].text
+            offerId = offer.find('{urn:1C.ru:commerceml_2}Ид').text
             if offerId in product_set:
-                currencies = offer[4]
+                currencies = offer.find('{urn:1C.ru:commerceml_2}Цены')
                 for currency in currencies:
-                    product_set[offerId].prices[currency[1].text] = currency[2].text
+                    product_set[offerId].prices[currency.find('{urn:1C.ru:commerceml_2}ИдТипаЦены').text] = currency.find('{urn:1C.ru:commerceml_2}ЦенаЗаЕдиницу').text
                 product_set[offerId].quantity = offer[5].text
-                if product_set[offerId].quantity == '0':
+                if not product_set[offerId].quantity_is_positive():
                     product_set[offerId].available = 'false'
-                size_data = size_finder(offer[2].text)
+                size_data = size_finder(offer.find('{urn:1C.ru:commerceml_2}Наименование').text)
                 if type(size_data) is list:
                     # create copy of current object but with another size
                     product_set[offerId+'_add_size'] = copy.deepcopy(product_set[offerId])
